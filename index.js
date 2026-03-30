59105185a2999048b6b852e502e6ec1737db8e01
oussamadrf <oussamadrifi107@gmail.com>
2026-03-30 15:19:37 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,4 @@
+// the alternative of window object in the browser is the "global" object
+// console.log(global)
+// some browser APIs are implemented in NodeJS like setTimeout
+// console.log(process) // informations about the process that's being run
